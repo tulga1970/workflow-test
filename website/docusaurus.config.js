89a00424a6d0ba76528245014191c4d414413a00
Tulga Narmandakh <tulgan@fb.com>
2022-06-17 15:16:24 -0700

    test

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -8,8 +8,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://tulga1970.github.io/workflow-test/',
-  baseUrl: '/',
+  url: 'https://tulga1970.github.io/',
+  baseUrl: 'workflow-test/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
